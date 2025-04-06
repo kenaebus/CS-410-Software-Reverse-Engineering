b51097acfbd8d4edae6320a5390f7857543890ef
Makena <kenaebustos@gmail.com>
2025-04-06 06:32:52 -0400

    Initial Commit

---
+++ Project1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void DisplayInfo(){
+    
+}
+
+
+void CheckUserPermissionAccess() {
+    string userName = "";
+    string password = "";
+
+
+
+}
+
+void ChangeCustomerChoice(){
+
+}
+
+
+int main() {
+    cout << "Created by: Makena Bustos" << endl;
+    cout <<"Hello! Welcome to our Investment Company" << endl;
+
+    return 0;
+}
